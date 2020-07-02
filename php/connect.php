d7cdb572361bca4c80b69f84eb39b5e4efa93964
juri0698 <50385154+juri0698@users.noreply.github.com>
2020-07-02 12:52:03 +0800

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,3 +1,3 @@
 <?php
-		$con7 = mysqli_connect('199.217.115.22', 'admin', 'AtmpI@2019', 'jobs') or die(mysqli_connect_error());
+		$con7 = mysqli_connect('localhost', 'root', '', 'jobs') or die(mysqli_connect_error());
 ?>
